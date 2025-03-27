f3368f85633185dd62d38c0893d9036523d83556
The-Best-Codes <bestcodes.official@gmail.com>
2025-03-27 17:03:32 -0400

    Update index.tsx

--- components/bcaptcha/index.tsx
+++ components/bcaptcha/index.tsx
@@ -25,7 +25,7 @@ const BCaptcha: React.FC<BCaptchaProps> = ({ onTokenReceived }) => {
       src="/bcaptcha"
       width="200"
       height="50"
-      className="border border-border rounded-md bg-background"
+      className="border-border rounded-md bg-background"
       title="BCaptcha"
       aria-label="Please click on the button to prove that you are not a robot"
     />
