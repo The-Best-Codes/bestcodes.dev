311a5327b5810ba19c6e20e4f0a5d892eada739f
The-Best-Codes <bestcodes.official@gmail.com>
2025-03-29 14:31:04 -0400

    fix: re-add header and footer to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import ClientLogger from "@/components/global/client-logger";
+import Footer from "@/components/global/footer";
+import Header from "@/components/global/header";
 import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -70,8 +72,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Header />
           <ClientLogger />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
