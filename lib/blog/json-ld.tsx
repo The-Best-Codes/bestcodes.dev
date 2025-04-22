2d232bc70950241118109e96c954dea1ec5b326d
The-Best-Codes <bestcodes.official@gmail.com>
2025-04-22 13:24:49 -0500

    fix dates to use iso in json ld

--- lib/blog/json-ld.tsx
+++ lib/blog/json-ld.tsx
@@ -7,30 +7,38 @@ export type JsonLdProps = {
 };
 
 export const JsonLd = ({ post, slug }: JsonLdProps) => {
-  const jsonLd = {
-    "@context": "https://schema.org",
-    "@type": "BlogPosting",
-    mainEntityOfPage: {
-      "@type": "WebPage",
-      "@id": relativeToAbsolute(`/blog/${slug}`),
-    },
-    url: relativeToAbsolute(`/blog/${slug}`),
-    headline: post.title,
-    image: [relativeToAbsolute(post?.image?.url)],
-    datePublished: post.date.created,
-    dateModified: post.date.updated,
-    author: {
-      "@type": "Person",
-      name: post.author.name,
-    },
-    description: post?.description || "Content on BestCodes Official Website",
-    isAccessibleForFree: true,
-  };
+  try {
+    const jsonLd = {
+      "@context": "https://schema.org",
+      "@type": "BlogPosting",
+      mainEntityOfPage: {
+        "@type": "WebPage",
+        "@id": relativeToAbsolute(`/blog/${slug}`),
+      },
+      url: relativeToAbsolute(`/blog/${slug}`),
+      headline: post.title,
+      image: [relativeToAbsolute(post?.image?.url)],
+      datePublished: new Date(post.date.created).toISOString(),
+      dateModified: post.date.updated
+        ? new Date(post.date.updated).toISOString()
+        : undefined,
+      author: {
+        "@type": "Person",
+        name: post.author.name,
+        url: relativeToAbsolute("/"),
+      },
+      description: post?.description || "Content on BestCodes Official Website",
+      isAccessibleForFree: true,
+    };
 
-  return (
-    <script
-      type="application/ld+json"
-      dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-    />
-  );
+    return (
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+    );
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };
