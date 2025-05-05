9d332d4aefc11fe33a2a3c83ba334f19ade070ec
The-Best-Codes <bestcodes.official@gmail.com>
2025-05-05 18:08:19 -0500

    add use client to button?? fix ref error???

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
@@ -40,10 +42,8 @@ function Button({
   size,
   asChild = false,
   ...props
-}:
-  & React.ComponentProps<"button">
-  & VariantProps<typeof buttonVariants>
-  & {
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
   }) {
   const Comp = asChild ? Slot : "button";
