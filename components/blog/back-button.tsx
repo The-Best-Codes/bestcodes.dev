1986ee6c2177ce897e24190a3bfaa29b8386ccd2
The-Best-Codes <bestcodes.official@gmail.com>
2025-05-04 20:59:06 -0500

    add suspense

--- components/blog/back-button.tsx
+++ components/blog/back-button.tsx
@@ -3,12 +3,22 @@ import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import { Skeleton } from "../ui/skeleton";
 
 interface BackButtonProps {
   defaultHref: string;
 }
 
 export function BackButton({ defaultHref }: BackButtonProps) {
+  return (
+    <Suspense fallback={<Skeleton className="w-40 h-8"></Skeleton>}>
+      <Component defaultHref={defaultHref} />
+    </Suspense>
+  );
+}
+
+function Component({ defaultHref }: BackButtonProps) {
   const searchParams = useSearchParams();
   const backButtonUrl = searchParams.get("backButtonUrl");
   let backButtonHref = defaultHref;
