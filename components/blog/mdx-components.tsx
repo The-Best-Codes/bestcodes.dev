09f5f3b6e4e7e33bca7cbe2f7bdea5d98da9c131
The-Best-Codes <bestcodes.official@gmail.com>
2025-06-04 11:03:11 -0500

    next/image in blog posts

--- components/blog/mdx-components.tsx
+++ components/blog/mdx-components.tsx
@@ -1,8 +1,40 @@
+import getDynamicImageAsStatic from "@/lib/getImageDynamic";
+import Image from "next/image";
 import OutboundLink from "../global/links/outbound";
 import { Button } from "../ui/button";
 import DevToEmbed from "./DevToEmbed";
-import { Margin } from "./Margin";
 import DiscordBotInvite from "./DiscordBotInvite";
+import { Margin } from "./Margin";
+
+const MdxImage = async (props: any) => {
+  if (typeof props.src !== "string" || !props.src) {
+    console.error("Invalid image src:", props.src);
+    return null;
+  }
+
+  const isBlogImage = props.src.startsWith("/image/blog");
+  const external = !isBlogImage;
+
+  let srcData = null;
+  if (!external) {
+    srcData = await getDynamicImageAsStatic(props.src, "public");
+  }
+
+  return (
+    <>
+      {!external && srcData ? (
+        <Image
+          src={srcData}
+          placeholder="blur"
+          alt={props.alt || ""}
+          loading="lazy"
+        />
+      ) : (
+        <img {...props} alt={props.alt || ""} loading="lazy" decoding="async" />
+      )}
+    </>
+  );
+};
 
 export const components = {
   DevToEmbed,
@@ -10,4 +42,5 @@ export const components = {
   OutboundLink,
   Margin,
   DiscordBotInvite,
+  img: MdxImage,
 };
