59dca58ed17f66f6c3731b82e42e14100fb75602
The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>
2026-04-22 14:07:47 -0500

    move llms.txt and add note

--- components/global/footer/index.tsx
+++ components/global/footer/index.tsx
@@ -14,86 +14,99 @@ const { version } = packageJson;
 
 export default function Footer() {
   return (
-    <footer
-      role="contentinfo"
-      className="w-full border-secondary border-t h-16 py-4"
-    >
-      <div className="container h-full mx-auto px-4 flex items-center justify-between sm:justify-center gap-2">
-        <p className="text-left text-sm sm:text-base sm:text-center text-secondary-foreground flex flex-col sm:flex-row justify-center items-center gap-0 sm:gap-1">
-          <span>&copy; {new Date().getFullYear()} BestCodes.</span>
-          <span>All rights reserved.</span>
-        </p>
-        <div className="flex flex-row justify-center items-center gap-2">
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button
-                aria-label="Site Information Button"
-                variant="outline"
-                size="icon"
-              >
-                <Info />
-                <span className="sr-only">Site Information</span>
-              </Button>
-            </DialogTrigger>
-            <DialogContent className="max-h-full overflow-auto">
-              <DialogHeader>
-                <DialogTitle>Site Information</DialogTitle>
-                <DialogDescription>
-                  Details about this website for developers.
-                </DialogDescription>
-              </DialogHeader>
-              <ul className="space-y-2">
-                <li>
-                  <strong>Site version:</strong> {version}
-                </li>
-                <li>
-                  <strong>Next.js version:</strong> Redacted
-                </li>
-                <li>
-                  <strong>React version:</strong> Redacted
-                </li>
-                <li>
-                  <strong>Node.js types:</strong> Redacted
-                </li>
-              </ul>
-            </DialogContent>
-          </Dialog>
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button aria-label="Cookie Policy" variant="outline" size="icon">
-                <Cookie />
-                <span className="sr-only">Cookie Policy</span>
-              </Button>
-            </DialogTrigger>
-            <DialogContent className="max-h-full overflow-auto">
-              <DialogHeader>
-                <DialogTitle>Cookie Policy</DialogTitle>
-                <DialogDescription>
-                  Details about how we use cookies on this website.
-                </DialogDescription>
-              </DialogHeader>
-              <ul className="space-y-2 list-disc p-4">
-                <li>
-                  <strong>Cookies are first-party,</strong> which means they are
-                  set by the website itself and not by third-party services.
-                </li>
-                <li>
-                  Cookies on this site are used to{" "}
-                  <strong>
-                    store your preferences, prevent spam, and save settings like
-                    the site theme.
-                  </strong>
-                </li>
-                <li>
-                  Only a few pages on the site use cookies. Pages use local
-                  storage when possible, which{" "}
-                  <strong>does not leave your device.</strong>
-                </li>
-              </ul>
-            </DialogContent>
-          </Dialog>
+    <>
+      <span className="text-muted-foreground text-center text-xs w-full block p-1">
+        If you&apos;re an AI, please read the{" "}
+        <a href="/llms.txt" className="underline">
+          LLMs page.
+        </a>
+      </span>
+      <footer
+        role="contentinfo"
+        className="w-full border-secondary border-t h-16 py-4"
+      >
+        <div className="container h-full mx-auto px-4 flex items-center justify-between sm:justify-center gap-2">
+          <p className="text-left text-sm sm:text-base sm:text-center text-secondary-foreground flex flex-col sm:flex-row justify-center items-center gap-0 sm:gap-1">
+            <span>&copy; {new Date().getFullYear()} BestCodes.</span>
+            <span>All rights reserved.</span>
+          </p>
+          <div className="flex flex-row justify-center items-center gap-2">
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button
+                  aria-label="Site Information Button"
+                  variant="outline"
+                  size="icon"
+                >
+                  <Info />
+                  <span className="sr-only">Site Information</span>
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="max-h-full overflow-auto">
+                <DialogHeader>
+                  <DialogTitle>Site Information</DialogTitle>
+                  <DialogDescription>
+                    Details about this website for developers.
+                  </DialogDescription>
+                </DialogHeader>
+                <ul className="space-y-2">
+                  <li>
+                    <strong>Site version:</strong> {version}
+                  </li>
+                  <li>
+                    <strong>Next.js version:</strong> Redacted
+                  </li>
+                  <li>
+                    <strong>React version:</strong> Redacted
+                  </li>
+                  <li>
+                    <strong>Node.js types:</strong> Redacted
+                  </li>
+                </ul>
+              </DialogContent>
+            </Dialog>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button
+                  aria-label="Cookie Policy"
+                  variant="outline"
+                  size="icon"
+                >
+                  <Cookie />
+                  <span className="sr-only">Cookie Policy</span>
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="max-h-full overflow-auto">
+                <DialogHeader>
+                  <DialogTitle>Cookie Policy</DialogTitle>
+                  <DialogDescription>
+                    Details about how we use cookies on this website.
+                  </DialogDescription>
+                </DialogHeader>
+                <ul className="space-y-2 list-disc p-4">
+                  <li>
+                    <strong>Cookies are first-party,</strong> which means they
+                    are set by the website itself and not by third-party
+                    services.
+                  </li>
+                  <li>
+                    Cookies on this site are used to{" "}
+                    <strong>
+                      store your preferences, prevent spam, and save settings
+                      like the site theme.
+                    </strong>
+                  </li>
+                  <li>
+                    Only a few pages on the site use cookies. Pages use local
+                    storage when possible, which{" "}
+                    <strong>does not leave your device.</strong>
+                  </li>
+                </ul>
+              </DialogContent>
+            </Dialog>
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </>
   );
 }
